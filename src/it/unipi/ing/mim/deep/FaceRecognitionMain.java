f77155efdc6d4fd8d77a788668792b4f968038e5
leoagueci <leo@MacBook-Pro-di-Leonardo.local>
2016-06-29 11:06:48 +0200

    features extraction

---
+++ src/it/unipi/ing/mim/deep/FaceRecognitionMain.java
@@ -0,0 +1,51 @@
+package it.unipi.ing.mim.deep;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class FaceRecognitionMain {
+
+	public static void main(String [] args) throws Exception{
+		
+		String imgPath1, imgPath2;
+	
+		FileReader source = new FileReader(Parameters.TEST_FILE);
+	    BufferedReader b = new BufferedReader(source);
+	 
+		DNNExtractor extractor = new DNNExtractor();
+		
+	    int count = Integer.parseInt(b.readLine());
+	    double [] distances = new double[count*2];
+	    
+	    for (int i = 0; i < (count*2); i++){
+	    	
+	    	String tmp = b.readLine();
+	    	String [] split = tmp.split("\\t");
+	    	
+	    	if (i < count){
+	    		imgPath1 = split[0] + "/" + split[0] + "_0" + ((Integer.parseInt(split[1]) < 10) ? "00" : (Integer.parseInt(split[1]) < 100) ? "0" : "") + split[1] + ".jpg";
+	    		imgPath2 = split[0] + "/" + split[0] + "_0" + ((Integer.parseInt(split[2]) < 10) ? "00" : (Integer.parseInt(split[2]) < 100) ? "0" : "") + split[2] + ".jpg";
+	    	}
+	    	else{
+	    		imgPath1 = split[0] + "/" + split[0] + "_0" + ((Integer.parseInt(split[1]) < 10) ? "00" : (Integer.parseInt(split[1]) < 100) ? "0" : "") + split[1] + ".jpg";
+	    		imgPath2 = split[2] + "/" + split[2] + "_0" + ((Integer.parseInt(split[3]) < 10) ? "00" : (Integer.parseInt(split[3]) < 100) ? "0" : "") + split[3] + ".jpg";
+	    	}   
+	    	
+	    	File img1 = new File(Parameters.SRC_FOLDER, imgPath1);
+			File img2 = new File(Parameters.SRC_FOLDER, imgPath2);
+			
+			float[] imgFeatures1 = extractor.extract(img1, Parameters.DEEP_LAYER);
+			float[] imgFeatures2 = extractor.extract(img2, Parameters.DEEP_LAYER);
+			
+			ImgDescriptor imgDescriptor1 = new ImgDescriptor(imgFeatures1, img1.getName());
+			ImgDescriptor imgDescriptor2 = new ImgDescriptor(imgFeatures2, img2.getName());
+			
+			distances[i] = imgDescriptor1.distance(imgDescriptor2);
+			System.out.println("distance " + i + " : " + distances[i]);
+	    }
+	
+		b.close();
+		source.close();
+	}
+}
---
+++ src/it/unipi/ing/mim/deep/ImgDescriptor.java
@@ -0,0 +1,81 @@
+package it.unipi.ing.mim.deep;
+
+import java.io.Serializable;
+
+public class ImgDescriptor implements Serializable, Comparable<ImgDescriptor> {
+
+	private static final long serialVersionUID = 1L;
+	
+	private float[] normalizedVector; // image feature
+	
+	public String id; // unique id of the image (usually file name)
+	
+	public double dist; // used for sorting purposes
+	
+	public ImgDescriptor(float[] features, String id) {
+		this.id = id;
+		float norm2 = evaluateNorm2(features);
+		this.normalizedVector = getNormalizedVector(features, norm2);
+	}
+	
+	public float[] getFeatures() {
+		return normalizedVector;
+	}
+	
+    // compare with other friends using distances
+	@Override
+	public int compareTo(ImgDescriptor arg0) {
+		return new Double(dist).compareTo(arg0.dist);
+	}
+	
+	//TODO
+	public double distance(ImgDescriptor desc) {
+		
+		double sum = 0;
+		double dist;
+		
+		//Evaluate the Euclidian distance between 2 ImgDescriptor
+		float [] normalizedVector2 = desc.getFeatures();
+		for (int i = 0; i < this.normalizedVector.length; i++){
+			
+			sum += Math.pow(this.normalizedVector[i] - normalizedVector2[i], 2);	
+		}
+		
+		dist = Math.sqrt(sum);
+		
+		return dist;
+	}
+	
+	//TODO
+	private float[] getNormalizedVector(float[] vector, float norm2) {
+		
+		float[] normalizedVector = new float [vector.length];
+		
+		//Normalize the vector values by means of its norm 2
+		
+		for(int i = 0; i < vector.length; i++){
+			
+			normalizedVector[i] = vector[i]/norm2;
+		}
+		
+		return normalizedVector;
+	}
+	
+	//TODO
+	private float evaluateNorm2(float[] vector) {
+		
+		float norm2 = 0;
+		double sum = 0;
+		
+		//Evaluate the norm 2 of the vector
+		for (int i = 0; i < vector.length; i++){
+			
+			sum += Math.pow(vector[i], 2);
+		}
+		
+		norm2 = (float)Math.sqrt(sum);
+		
+		return norm2;
+	}
+    
+}
